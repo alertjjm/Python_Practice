c187ff25e9e1b601eb449fa6fa901136f0a368b6
alertjjm <sdr2936@gmail.com>
2020-08-30 17:09:19 +0900

    [BAEKJOON]

--- 2292.py
+++ 2292.py
@@ -1,3 +1,14 @@
 n=int(input())
-t=(n-1)//6+1
-print(t)
+if n==1:
+    print(1)
+else:
+    mi=2
+    ma=7
+    i=1
+    while True:
+        if n>=mi and n<=ma:
+            break
+        mi+=6*(i)
+        ma+=6*(i+1)
+        i+=1
+    print(i+1)
