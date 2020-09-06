02404deb9e6cc23367f77fbf3d3d5645882fc9f5
alertjjm <sdr2936@gmail.com>
2020-09-07 00:37:17 +0900

    [BAEKJOON]

--- 15649.py
+++ 15649.py
@@ -2,8 +2,22 @@ n,m=map(int, input().split(" "))
 out=[]
 visited=[False]*n
 a=[0]*m
-
+result=[]
 def permu(index, n,m):
     if index==m:
-        ' '.join(map(str, out))
+        temp=set(out)
+        result.append(' '.join(map(str, temp)))
+        return
+    for i in range(len(visited)):
+        if not visited[i]:
+            visited[i]=True
+            out.append(i+1)
+            permu(index+1,n,m)
+            visited[i]=False
+            out.pop()
+permu(0,n,m)
+result=list(set(result))
+result.sort()
 
+for i in result:
+    print(i)
